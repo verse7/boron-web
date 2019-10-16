3d77bc4850c9fdf1c950945688b344c53f4ba60b
Gabrielle-Joy <gabzi.contact@gmail.com>
2019-10-16 12:57:20 -0500

    add grid to suborders

--- src/components/Order.vue
+++ src/components/Order.vue
@@ -3,9 +3,9 @@
   <h1>Order Title</h1>
   <div class="orders-container">
     <SubOrder v-for="i in [1,2,3]" :key="i"/>
-    <div class="d-flex justify-between p-4">
+    <div class="d-flex grid justify-between p-4">
       <div class="name">Gabrielle Higgins</div>
-      <div class="order-desc">Medium Oven Jerk Chicken w/ Rice and Peas and Steam Veg</div>
+      <div class="order-desc">Medium Oven Jerk Chicken w/ Banana and Pumpkin and curry gravy and Pasta</div>
       <div class="cost">$450</div>
     </div>
   </div>
@@ -32,6 +32,13 @@ export default {
   height: auto;
   border: 1px solid pink;
   border-radius: 16px;
+}
+
+.grid {
+  display: grid;
+  grid-template-columns: 1fr 3fr 1fr;
+  grid-column-gap: 10px;
+  justify-items: center;
 }
 
 
--- src/components/SubOrder.vue
+++ src/components/SubOrder.vue
@@ -1,9 +1,10 @@
 <template>
-<div class="d-flex justify-between p-4">
+<div class="d-flex grid justify-between p-4">
   <div class="name">Zidane Wright</div>
   <div class="order-desc">Medium Oven Jerk Chicken w/ Rice and Peas and Steam Veg</div>
   <div class="cost">$450</div>
 </div>
+
 </template>
 
 <script lang="ts">
@@ -15,4 +16,10 @@ export default class SubOrder extends Vue {}
 
 <style lang="scss" scoped>
 
+.grid {
+  display: grid;
+  grid-template-columns: 1fr 3fr 1fr;
+  grid-column-gap: 10px;
+  justify-items: center;
+}
 </style>
