3e73d4753eddcaf508b57df3fc5bab36ede6b714
Rowan Atkinson <rowaneatkinson@gmail.com>
2019-12-05 16:40:13 -0500

    init branch commit

---
+++ src/components/Login.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <div class="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></div>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+    data() {
+        return {
+
+        };
+    },
+    head: {
+        meta: [
+            { name: "google-signin-scope", content: "profile email" },
+            { name: "google-signin-client_id", content: "YOUR_CLIENT_ID.apps.googleusercontent.com" },
+        ],
+        script: [
+            { src: "https://apis.google.com/js/platform.js", async: true, defer: "true"},
+        ],
+    },
+    methods: {
+        onSignIn(googleUser: any) {
+            // Useful data for your client-side scripts:
+            const profile = googleUser.getBasicProfile();
+            console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+            console.log('Full Name: ' + profile.getName());
+            console.log('Given Name: ' + profile.getGivenName());
+            console.log('Family Name: ' + profile.getFamilyName());
+            console.log("Image URL: " + profile.getImageUrl());
+            console.log("Email: " + profile.getEmail());
+
+            // The ID token you need to pass to your backend:
+            const id_token = googleUser.getAuthResponse().id_token;
+            console.log("ID Token: " + id_token);
+      },
+    },
+});
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
+import Login from '../components/Login.vue';
 import Menu from '../components/Menu.vue';
 
 Vue.use(VueRouter);
@@ -10,6 +11,11 @@ const routes = [
     path: '/',
     name: 'home',
     component: Menu,
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login,
   },
 ];
 
